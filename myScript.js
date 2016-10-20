5cccd6e4e0e44955a47fdec7ed0568b41b9167c7
Wesley Tsang <k1504155@kcl.ac.uk>
2016-10-21 00:12:56 +0100

    Width edited

--- myScript.js
+++ myScript.js
@@ -2,11 +2,6 @@
  * Created by Wesley on 15/10/2016.
  */
 
-function project() {
-    var popup = document.getElementById('myPopup');
-    popup.classList.toggle('show');
-}
-
 function dropDown() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {

d7542e6355fcca04413dd1723d5ae6a0fbe134e9
Wesley Tsang <k1504155@kcl.ac.uk>
2016-10-20 21:44:18 +0100

    NavBar updated

--- myScript.js
+++ myScript.js
@@ -6,3 +6,12 @@ function project() {
     var popup = document.getElementById('myPopup');
     popup.classList.toggle('show');
 }
+
+function dropDown() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}

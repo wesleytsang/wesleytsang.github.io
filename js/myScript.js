1e35eaa1eeac9243bc74376c42fa2b6261319b99
Wesley Tsang <k1504155@kcl.ac.uk>
2016-10-21 23:11:16 +0100

    2nd commit

--- js/myScript.js
+++ js/myScript.js
@@ -38,40 +38,49 @@ $(document).ready(function(){
         });
       });
 
-var ctx = document.getElementById("myChart");
+const ctx = document.getElementById("myChart");
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
+        labels: ["Programing", "Web Design", "Unique Idea", "Team Player", "Quick Learner"],
+        datasets: [
+            {
+                type: 'bar',
+                label: 'Bar Component',
+                data: [10, 6, 9, 7, 8],
+            },
+        ]
     },
     options: {
         scales: {
             yAxes: [{
+                stacked: true,
+                gridLines: {
+                    display: false,
+                    drawBorder: false
+                },
                 ticks: {
-                    beginAtZero:true
+                    fontSize: 0
+                }
+            }],
+            xAxes: [{
+                gridLines: {
+                    display: false
+                },
+                ticks: {
+                    fontSize: 18
                 }
             }]
+        },
+        legend: {
+          display: false
+        },
+        tooltips: {
+        callbacks: {
+           label: function(tooltipItem) {
+                  return tooltipItem.yLabel;
+           }
         }
+    }
     }
 });
